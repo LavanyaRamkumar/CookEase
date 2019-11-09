077454fe62d852c4f2ce93ed7e2eece741d23ccc
KavyaVarma <kavya.varma98@gmail.com>
2019-11-09 11:13:06 +0530

    Changes to server

--- server/server.py
+++ server/server.py
@@ -1,15 +1,64 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 import pickle
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
+def match(recipe, pantry):
+	total = len(recipe["ingredients"])
+	available = 0
+	for ingredient in recipe["ingredients"]:
+		if ingredient in pantry:
+			available+=1
+	return available/total
+
 class Recipes(Resource):
 	def get(self):
-		return recipes[list(recipes.keys())[0]]
+		args = request.form
+		cuisine_query = args["cuisine"]
+		type_query = args["type"] 
+		pantry = args.getlist("Ingredients")
+
+		if pantry==None:
+			pantry = []
+
+		answers = list(recipes.keys())
+
+		if cuisine_query!="":
+			temp_answers = []
+			for key in answers:
+				if recipes[key]["cuisine"]==cuisine_query:
+					temp_answers.append(key)
+			answers = temp_answers
+
+		if type_query!="":
+			temp_answers = []
+			for key in answers:
+				if recipes[key]["type"]==type_query:
+					temp_answers.append(key)
+			answers = temp_answers
+
+		probability = []
+		for key in answers:			
+			probability.append((match(recipes[key], pantry), key))
+		probability.sort(reverse=True)
+		return [probability, [recipes[key] for _, key in probability]]
+
+class RecipesId(Resource):
+	def get(self):
+		args = request.form 
+		id_list = args.getlist("id")
+
+		answer = []
+
+		for num in id_list:
+			answer.append(recipes[num])
+		return answer
 
 api.add_resource(Recipes, '/cookease/recipes/')
+api.add_resource(RecipesId, '/cookease/recipes/id/')
 
 if __name__ == '__main__':
 	with open("../data/ingredients.pkl", "rb") as file:
--- server/test_commands.sh
+++ server/test_commands.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl http://127.0.0.1:5000/cookease/recipes/ -X GET -d '{"type":"appetisers", cuisine:"Indian"}'
+curl http://127.0.0.1:5000/cookease/recipes/ -X GET \
+-d 'type=appetisers&cuisine=Indian&Ingredients=tomato&Ingredients=potato&Ingredients=chickpeas&Ingredients=salt&Ingredients=lemon'
