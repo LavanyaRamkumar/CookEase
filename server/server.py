f1852b41b1d74b8ed95a987731e2244121c80fcf
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2019-11-20 21:47:54 +0530

    directory changes ..changed assets folder path

--- server/server.py
+++ server/server.py
@@ -15,9 +15,17 @@ cors = CORS(app)
 def index():
 	return (render_template("index.html"))
 
+# @app.route("/menu.html")
+# def menu():
+# 	return (render_template("menu.html"))
+
 @app.route("/pf")
 def pf():
 	return (render_template("pf_msd.html"))
+
+@app.route("/groceries")
+def groceries():
+	return (render_template("trial_grocery.html"))
 	
 def match(recipe, pantry):
 	total = len(recipe["ingredients"])
--- server/static/assets/our/msd.js
+++ server/static/assets/our/msd.js
@@ -10,8 +10,8 @@ function get_cuisines(){
     if(this.readyState == 4 && this.status == 200) {
         var res = JSON.parse(this.responseText);
         var listing = document.getElementById("cuidiv");
-        var card_init='<div class="card col-12 col-md-6 col-lg-3"> <div class="icon-block"><a href="listing.html" id="'
-        var card_mid_1='"></a></div><h5 class="mbr-fonts-style display-5">'
+        var card_init='<div class="card col-12 col-md-6 col-lg-3"> <div class="icon-block"  style="height:112px; width:315px;"><a href="listing.html" id="'
+        var card_mid_1='"></a></div><h5 class="mbr-fonts-style display-5" style="height:112px; width:315px;">'
         var card_end='<br></h5></div>'
         for (x in res){
             var spl=[x,res[x]];
@@ -32,6 +32,6 @@ function get_cuisines_images(id,img){
     // console.log(img,id)
     var ele=document.getElementById(id);
     // console.log(ele)
-    ele.innerHTML+='<img src="assets/images/cuisines/'+img+'" class = "cuisine-images-css"></img>'
+    ele.innerHTML+='<img src="../static/assets/images/cuisines/'+img+'" class = "cuisine-images-css"></img>'
 }
 multiInit();
