1e3c57f88896949e5400cf78188c8f0ccb612a30
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2019-11-22 19:29:48 +0530

    add recipe

---
+++ server/new_data.sh
@@ -0,0 +1,4 @@
+rm 'static/data/categorical_ingredients'
+rm 'static/data/id_recipes.pkl'
+cp '../data_not_using/categorical_ingredients' 'static/data/categorical_ingredients'
+cp '../data_not_using/id_recipes.pkl' 'static/data/id_recipes.pkl'
